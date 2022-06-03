7481b5e27c6be785e38835df6c385a327bce59d6
minjeong-dev <vitaminjeong1026@gmail.com>
2022-06-04 01:29:15 +0900

    메인페이지 틀잡기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Main from './Main';
+import Filter from './routes/Filter';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+        <Route path='/filter' element={<Filter />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,30 @@
+import { Link } from 'react-router-dom';
+import './Main.css';
 function Main() {
   return (
     <>
-      <header></header>
-      <aside></aside>
-      <section></section>
+      <header>김민정님, 환영합니다!</header>
+      <main>
+        <aside>
+          <h3>메뉴</h3>
+          <ul>
+            <li>
+              <Link to={'/filter/korean'}>한식</Link>
+            </li>
+            <li>
+              <Link to={'/filter/japanese'}>일식</Link>
+            </li>
+            <li>
+              <Link to={'/filter/chinese'}>중식</Link>
+            </li>
+            <li>
+              <Link to={'/filter/western'}>양식</Link>
+            </li>
+          </ul>
+          <input type='text'></input>
+        </aside>
+        <section>전체 게시물</section>
+      </main>
     </>
   );
 }
